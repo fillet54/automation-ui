3c736bac2e9d86d8b9be95766a7db112cfca764e
Phillip Gomez <gomez.phillip@gmail.com>
2022-02-12 14:14:23 -0800

    Editor in work

--- public/src/Components/Editor.jsx
+++ public/src/Components/Editor.jsx
@@ -2,12 +2,11 @@ import React, { useState, useEffect } from "react";
 
 const CodeEditor = props => {
     const [content, setContent] = useState(props.content);
+    const [highlighted, setHighlighted] = useState(props.content);
 
     const handleKeyDown = evt => {
         let value = content,
             selStartPos = evt.currentTarget.selectionStart;
-
-        console.log(evt.currentTarget);
 
         // handle 4-space indent on
         if (evt.key === "Tab") {
@@ -24,21 +23,34 @@ const CodeEditor = props => {
     };
 
     useEffect(() => {
-        //Prism.highlightAll();
+        if (content) {
+            let tmp = content;
+
+            /* syntax highlighting */
+            const blockReg = /^([^\s]+)/
+            tmp = tmp.replace(blockReg, '<span class="text-blue-500">$1</span>');
+            setHighlighted(tmp);
+        }
+        else {
+            setHighlighted('')
+        }
     }, [props.language, content]);
 
     return (
-        <div className="code-edit-container" style={{ position: 'relative' }}>
-            <textarea
-                className="code-input"
-                value={content}
-                onChange={evt => setContent(evt.target.value)}
-                onKeyDown={handleKeyDown}
-                style={{ position: 'absolute' }}
-            />
-            <pre className="code-output" style={{ position: 'relative' }}>
-                <code className={`language-${props.language}`}>{content}</code>
-            </pre>
+        <div className="overflow-auto" style={{ maxHeight: 400 }}>
+            <div className="relative" style={{ width: 400 }}>
+                <textarea
+                    className="text-black text-opacity-0 w-full text-base font-mono absolute bg-gray-100 px-2 overflow-hidden"
+                    value={content}
+                    onChange={evt => setContent(evt.target.value)}
+                    onKeyDown={handleKeyDown}
+                    spellCheck={false}
+                    style={{ caretColor: 'black', outline: 0 }}
+                />
+                <pre className="pointer-events-none relative whitespace-pre-wrap w-full text-base font-mono px-2 overflow-hidden"
+                    dangerouslySetInnerHTML={{ __html: highlighted }}>
+                </pre>
+            </div>
         </div>
     );
 };
