4f37c40d7fc92f1c047d1e05c396cd6c428c223a
Phillip Gomez <gomez.phillip@gmail.com>
2022-09-02 09:35:54 -0700

    Continuing work on introduction

---
+++ clean.bat
@@ -0,0 +1,4 @@
+jupyter nbconvert "01 - Introduction.ipynb" --to notebook --ClearOutputPreprocessor.enabled=True --output "01 - Introduction.ipynb" 
+jupyter nbconvert "02 - The Reader.ipynb" --to notebook --ClearOutputPreprocessor.enabled=True --output "02 - The Reader.ipynb" 
+jupyter nbconvert "Appendix - A - Literate Programming.ipynb" --to notebook --ClearOutputPreprocessor.enabled=True --output "Appendix - A - Literate Programming.ipynb" 
+jupyter nbconvert "Appendix - B - Utilities.ipynb" --to notebook --ClearOutputPreprocessor.enabled=True --output "Appendix - B - Utilities.ipynb" 
